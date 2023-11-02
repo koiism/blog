cbe215636a5cef48c0be7cd943f713ef92e1ed48
pidanmeng <1092333914@qq.com>
2023-11-03 01:30:27 +0800

    feat: 🎸 init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import UnoCSS from 'unocss/astro';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), UnoCSS(), mdx()]
 });
