f21e84f2f6a8b0feb1b220f208649eb116c7fa50
pidanmeng <1092333914@qq.com>
2023-11-05 09:16:09 +0800

    feat: 🎸 init

--- src/schemas/posts.ts
+++ src/schemas/posts.ts
@@ -1,18 +1,19 @@
-
 import { z, defineCollection, reference } from 'astro:content';
 export const posts = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()),
-    images: z.optional(
-      z.array(
-        z.object({
-          src: z.string(),
-          desc: z.string(),
-        })
-      )
-    ),
-    author: reference('authors'),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      tags: z.array(z.string()),
+      images: z
+        .array(
+          z.object({
+            src: image(),
+            desc: z.string(),
+          })
+        )
+        .optional(),
+      cover: image().optional(),
+      author: reference('authors'),
+    }),
 });
